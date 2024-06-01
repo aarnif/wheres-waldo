a565440533cfead48751527660a4debdf9065dc0
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-01 11:13:57 +0300

    Add colorTheme to Game-model

--- models/Game.js
+++ models/Game.js
@@ -6,6 +6,19 @@ const gameSchema = new Schema({
   title: { type: String, required: true },
   image: { type: String, required: true },
   characters: [{ type: Schema.Types.ObjectId, ref: "Character" }],
+
+  colorTheme: {
+    body: { type: String, required: true },
+    gameCardBackground: { type: String, required: true },
+    gameCard: { type: String, required: true },
+    gameCardHover: { type: String, required: true },
+    gameCanvas: { type: String, required: true },
+    gameModal: { type: String, required: true },
+    gameButton: { type: String, required: true },
+    gameButtonHover: { type: String, required: true },
+    gameHeader: { type: String, required: true },
+  },
+
   leaderboard: [
     {
       user: { type: Schema.Types.ObjectId, ref: "User" },
