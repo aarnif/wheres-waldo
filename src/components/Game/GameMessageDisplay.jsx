dbfadff6a2f491ea2316224ddae26a7e3a89f0cb
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-08-22 16:46:44 +0300

    Update game message display styles

--- src/components/Game/GameMessageDisplay.jsx
+++ src/components/Game/GameMessageDisplay.jsx
@@ -1,19 +1,23 @@
+import utils from "../../utils";
 import { motion } from "framer-motion";
 
-const GameMessageDisplay = ({ gameMessage }) => {
+const GameMessageDisplay = ({ colorTheme, gameMessage }) => {
   return (
-    <motion.div className="fixed inset-0 flex justify-end items-end">
+    <motion.div className="fixed inset-0 flex justify-center items-end">
       <motion.div
+        style={{
+          backgroundColor: utils.hexToRgba(colorTheme.gameMessageBox, 0.8),
+        }}
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 50 }}
         transition={{ duration: 0.3 }}
-        className="m-8 p-8 bg-white bg-opacity-60 rounded-xl z-10"
+        className="m-8 p-8 rounded-xl z-10"
       >
         <h1
-          className="text-slate-800 font-bold text-4xl"
+          className="font-bold text-4xl text-white"
           style={{
-            color: gameMessage.includes("That is not") ? "red" : "green",
+            color: gameMessage.includes("That is not") ? "#ef4444" : "#22c55e",
           }}
         >
           {gameMessage.toUpperCase()}
