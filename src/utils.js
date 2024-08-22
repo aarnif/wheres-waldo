fb70a4c775dab3edbbf221b257e77e9f713e7a8b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-08-22 16:44:26 +0300

    Add hexToRgba-method to utils

--- src/utils.js
+++ src/utils.js
@@ -12,4 +12,14 @@ const formatTime = (time) => {
   return minutes + ":" + pad(seconds, 2) + ":" + deciseconds + centiseconds;
 };
 
-export default { formatTime };
+const hexToRgba = (hex, alpha = 1) => {
+  hex = hex.replace(/^#/, "");
+
+  let r = parseInt(hex.substring(0, 2), 16);
+  let g = parseInt(hex.substring(2, 4), 16);
+  let b = parseInt(hex.substring(4, 6), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
+
+export default { formatTime, hexToRgba };
