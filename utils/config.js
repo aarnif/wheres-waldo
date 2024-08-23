bd666a4974a2505c15b285b0bfa521a38ef90c52
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-08-23 10:08:22 +0300

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,10 @@
 import "dotenv/config";
 
 const PORT = process.env.PORT || 5000;
-const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const SESSION_SECRET = process.env.SESSION_SECRET;
-const MONGODB_URI = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@cluster0.xorb2be.mongodb.net/database?retryWrites=true&w=majority`;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 export default { MONGODB_URI, SESSION_SECRET, PORT };
