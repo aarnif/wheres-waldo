890891828fe1e0d21c90ef415f81453b14eeec03
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-09-03 19:50:03 +0300

    Update games-view style

--- src/components/Games/index.jsx
+++ src/components/Games/index.jsx
@@ -1,40 +1,69 @@
 import GameCard from "./GameCard.jsx";
 
+import { useState } from "react";
 import Icon from "@mdi/react";
-import { mdiChevronDoubleDown } from "@mdi/js";
+import { mdiChevronDoubleLeft, mdiChevronDoubleRight } from "@mdi/js";
 
 import { motion } from "framer-motion";
 
 const Games = ({ user, games }) => {
+  const [currentGameIndex, setCurrentGameIndex] = useState(0);
+  console.log("Render game card titled:", games[currentGameIndex].title);
+
+  const handlePreviousGame = () => {
+    setCurrentGameIndex((prevIndex) =>
+      prevIndex === 0 ? prevIndex : prevIndex - 1
+    );
+  };
+
+  const handleNextGame = () => {
+    setCurrentGameIndex((prevIndex) =>
+      prevIndex === games.length - 1 ? prevIndex : prevIndex + 1
+    );
+  };
+
   return (
-    <div
-      className="w-full flex-grow flex flex-col justify-center items-center bg-red-500"
-      style={{
-        backgroundColor: games[0].colorTheme.gameCanvas,
-      }}
-    >
-      <motion.div
-        initial={{ opacity: 0, y: -50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-        exit={{ opacity: 0, y: -50 }}
-        className="w-full flex-grow mt-32 flex flex-col justify-center items-center text-white"
+    <div className="w-full flex-grow flex justify-center items-center backdrop-blur-sm">
+      <motion.button
+        initial={{ opacity: 0, x: 30 }}
+        animate={{ opacity: 1, x: 0 }}
+        whileHover={{ opacity: 1, x: 0 }}
+        whileTap={{ opacity: 0, x: -15 }}
+        exit={{ opacity: 0, x: 30 }}
+        className="w-full flex justify-center items-center text-white"
+        onClick={handlePreviousGame}
       >
-        <h1 className="mb-8 text-5xl font-bold">Scroll Down To Play</h1>
-        <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, repeat: Infinity }}
-        >
-          <Icon path={mdiChevronDoubleDown} size={5} className="fill-current" />
-        </motion.div>
-      </motion.div>
-      {games.map((game, index) => {
-        if (index <= user.playedGames.length) {
-          console.log("Render game card titled:", game.title);
-          return <GameCard key={game.id} user={user} game={game} />;
-        }
-      })}
+        {currentGameIndex > 0 && (
+          <Icon
+            path={mdiChevronDoubleLeft}
+            size={5}
+            className="fill-current text-white hover:text-zinc-500 transition"
+          />
+        )}
+      </motion.button>
+      <GameCard
+        key={games[currentGameIndex].id}
+        user={user}
+        game={games[currentGameIndex]}
+      />
+
+      <motion.button
+        initial={{ opacity: 0, x: -30 }}
+        animate={{ opacity: 1, x: 0 }}
+        whileHover={{ opacity: 1, x: 0 }}
+        whileTap={{ opacity: 0, x: 15 }}
+        exit={{ opacity: 0, x: -30 }}
+        className="w-full flex justify-center items-center text-white"
+        onClick={handleNextGame}
+      >
+        {currentGameIndex < games.length - 1 && (
+          <Icon
+            path={mdiChevronDoubleRight}
+            size={5}
+            className="fill-current text-white hover:text-zinc-500 transition"
+          />
+        )}
+      </motion.button>
     </div>
   );
 };
