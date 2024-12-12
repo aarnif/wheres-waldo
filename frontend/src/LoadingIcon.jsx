c8b0189e6717d56f1c543178a3bf855097819751
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-12 18:10:37 +0200

    Add loading text to LoadingIcon.jsx

--- frontend/src/LoadingIcon.jsx
+++ frontend/src/LoadingIcon.jsx
@@ -2,24 +2,27 @@ import Icon from "@mdi/react";
 import { mdiCircleOutline, mdiLoading } from "@mdi/js";
 import { motion } from "framer-motion";
 
-const LoadingIcon = () => {
+const LoadingIcon = ({ loadingText = "" }) => {
   return (
-    <motion.div
-      animate={{ rotate: [0, 90, 180, 270, 360] }}
-      transition={{ repeat: Infinity }}
-      className="relative flex justify-center items-center"
-    >
-      <Icon
-        path={mdiCircleOutline}
-        size={3}
-        className="absolute fill-current text-zinc-800 opacity-70"
-      />
-      <Icon
-        path={mdiLoading}
-        size={3}
-        className="absolute fill-current text-white"
-      />
-    </motion.div>
+    <div className="relative w-full h-full flex flex-col justify-center items-center">
+      <motion.div
+        animate={{ rotate: [0, 90, 180, 270, 360] }}
+        transition={{ repeat: Infinity }}
+        className="relative flex justify-center items-center"
+      >
+        <Icon
+          path={mdiCircleOutline}
+          size={3}
+          className="absolute fill-current text-zinc-800 opacity-70"
+        />
+        <Icon
+          path={mdiLoading}
+          size={3}
+          className="absolute fill-current text-white"
+        />
+      </motion.div>
+      <div className="relative top-12 text-slate-200">{loadingText}</div>
+    </div>
   );
 };
 
