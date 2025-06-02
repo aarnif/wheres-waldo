fece01c6b20b99bc1c63c54a537ab581b06f5ebf
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-02 22:01:51 +0300

    Add mark to game card if completed

--- frontend/src/components/Games.jsx
+++ frontend/src/components/Games.jsx
@@ -3,9 +3,13 @@ import { useNavigate } from "react-router-dom";
 import baseUrl from "../../baseUrl.js";
 import GameCharacter from "./GameCharacter.jsx";
 
-const GameCard = ({ game }) => {
+const GameCard = ({ user, game }) => {
   const navigate = useNavigate();
   const { id, title, characters } = game;
+
+  const checkIfUserPlayed = game.leaderboard.find(
+    (entry) => entry.username === user.username
+  );
 
   const imageUrl = `${baseUrl}/games/${id}/image`;
   return (
@@ -16,7 +20,9 @@ const GameCard = ({ game }) => {
       <img
         src={imageUrl}
         alt={title}
-        className="w-full h-full object-cover rounded-lg"
+        className={`w-full h-full object-cover rounded-lg ${
+          !checkIfUserPlayed && "blur-md"
+        }`}
       />
       <div className="opacity-0 hover:opacity-100 absolute inset-0 p-12 flex flex-col justify-between bg-black/50 text-white rounded-lg transition-all duration-300 ease-in-out">
         <h2 className="text-2xl font-bold">{title}</h2>
@@ -33,15 +39,37 @@ const GameCard = ({ game }) => {
           Play Game
         </button>
       </div>
+      {checkIfUserPlayed && (
+        <div className="absolute inset-0 p-12 flex flex-col justify-center items-center rounded-lg bg-black/50 gap-8">
+          <h2 className="text-3xl font-bold text-slate-50 font-roboto-condensed">
+            Completed!
+          </h2>
+          <div className="flex flex-col items-center gap-2">
+            <h2 className="text-2xl font-bold text-slate-50 font-roboto-condensed">
+              Your time:{" "}
+            </h2>
+            <h2 className="text-3xl font-bold text-slate-50 font-mono">
+              {
+                game.leaderboard.find(
+                  (entry) => user.username === entry.username
+                ).time
+              }
+            </h2>
+          </div>
+          <button className="px-7 py-3 shadow-lg border border-red-600 bg-red-600 hover:bg-red-700 hover:border-red-700 active:border-red-800 active:inset-shadow-sm rounded-2xl text-white text-base font-bold transition-all duration-200 ease-in-out">
+            Play Again
+          </button>
+        </div>
+      )}
     </button>
   );
 };
 
-const Games = ({ games }) => {
+const Games = ({ user, games }) => {
   return (
     <div className="py-16 w-full max-w-[1800px] flex-grow flex justify-center items-center flex-wrap gap-16">
       {games.map((game) => (
-        <GameCard key={game.id} game={game} />
+        <GameCard key={game.id} user={user} game={game} />
       ))}
     </div>
   );
