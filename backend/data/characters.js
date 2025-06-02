f00ac5e19bcb497e1d5f93127fe56e34bae6346a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-02 14:59:31 +0300

    Add displayName-field to Character-Model

--- backend/data/characters.js
+++ backend/data/characters.js
@@ -1,18 +1,26 @@
 const characters = [
-  { _id: "66601f7a66990e4269078fd0", name: "Waldo", image: "waldo.png" },
+  {
+    _id: "66601f7a66990e4269078fd0",
+    name: "Waldo",
+    displayName: "Waldo",
+    image: "waldo.png",
+  },
   {
     _id: "66601f7a66990e4269078fd3",
     name: "Wenda",
+    displayName: "Wenda",
     image: "wenda.png",
   },
   {
     _id: "66601f7a66990e4269078fd1",
     name: "Odlaw",
+    displayName: "Odlaw",
     image: "odlaw.png",
   },
   {
     _id: "66601f7a66990e4269078fd2",
     name: "Wizard Whitebeard",
+    displayName: "Wizard",
     image: "wizard-whitebeard.png",
   },
 ];
--- backend/models/Character.js
+++ backend/models/Character.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const characterSchema = new Schema({
   name: { type: String, required: true },
+  displayName: { type: String, required: true },
   image: { type: String, required: true },
 });
 
