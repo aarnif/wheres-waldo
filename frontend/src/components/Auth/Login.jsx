22522d481641165a5a56fc20023bea4a5f085b13
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-04 11:16:50 +0300

    Make sign in page responsive

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,6 +1,5 @@
 import signingService from "../../services/signingService";
 import useField from "../../hooks/useField";
-import Title from "../Title";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -53,90 +52,91 @@ const Login = ({ setUser }) => {
   };
 
   return (
-    <div className="w-full flex-grow flex flex-col justify-center items-center">
-      <div className="w-full flex-grow flex flex-col">
-        <div className="flex-grow w-full flex justify-center items-center">
-          <div
-            className="flex-grow max-w-[500px] max-h-[600px] p-8 flex justify-center items-center
+    <div className="flex-grow w-full flex justify-center items-center">
+      <div
+        className="m-4 flex-grow max-w-[500px] max-h-[600px] p-4 sm:p-8 flex justify-center items-center
          bg-slate-700 bg-opacity-90 rounded-xl text-white shadow-xl"
+      >
+        <form
+          onSubmit={handleSubmit}
+          className="flex-grow h-full flex flex-col"
+        >
+          <h1
+            className="text-xl sm:text-2xl font-bold"
+            data-testid="sign-in-header"
           >
-            <form
-              onSubmit={handleSubmit}
-              className="flex-grow h-full flex flex-col"
-            >
-              <h1 className="text-2xl font-bold" data-testid="sign-in-header">
-                Sign In
-              </h1>
-              <ul>
-                {errorMessage && (
-                  <li className="p-2 flex justify-center items-center bg-red-400 rounded-lg">
-                    <span className="text-md text-red-700">{errorMessage}</span>
-                  </li>
-                )}
+            Sign In
+          </h1>
+          <ul>
+            {errorMessage && (
+              <li className="p-2 flex justify-center items-center bg-red-400 rounded-lg">
+                <span className="text-sm sm:text-base text-red-700">
+                  {errorMessage}
+                </span>
+              </li>
+            )}
 
-                <li className="my-4 w-full flex-grow flex flex-col">
-                  <label className="text-md font-medium text-slate-200">
-                    USERNAME:
-                  </label>
-                  <input
-                    data-testid="username-input"
-                    className="w-full flex-grow p-2 rounded-lg bg-slate-500
+            <li className="my-2 sm:my-4 w-full flex-grow flex flex-col">
+              <label className="text-sm sm:text-base font-medium text-slate-200">
+                USERNAME:
+              </label>
+              <input
+                data-testid="username-input"
+                className="w-full flex-grow p-2 rounded-lg bg-slate-500 text-sm sm:text-base
                   focus:outline-none focus:ring-2 focus:ring-sky-400 placeholder:text-slate-200 transition"
-                    {...userName}
-                  />
-                  {userName.value.length === 0 && (
-                    <span className="text-sm text-red-500">
-                      Please enter your username
-                    </span>
-                  )}
-                </li>
+                {...userName}
+              />
+              {userName.value.length === 0 && (
+                <span className="text-sm text-red-500">
+                  Please enter your username
+                </span>
+              )}
+            </li>
 
-                <li className="my-4 w-full flex-grow flex flex-col">
-                  <label className="text-md font-medium text-slate-200">
-                    PASSWORD:
-                  </label>
-                  <input
-                    data-testid="password-input"
-                    className="w-full flex-grow p-2 rounded-lg bg-slate-500
+            <li className="my-2 sm:my-4 w-full flex-grow flex flex-col">
+              <label className="text-sm sm:text-base font-medium text-slate-200">
+                PASSWORD:
+              </label>
+              <input
+                data-testid="password-input"
+                className="w-full flex-grow p-2 rounded-lg bg-slate-500 text-sm sm:text-base
           focus:outline-none focus:ring-2 focus:ring-sky-400 placeholder:text-slate-200 transition"
-                    {...password}
-                  />
-                  {password.value.length === 0 && (
-                    <span className="text-sm text-red-500">
-                      Please enter your password
-                    </span>
-                  )}
-                </li>
+                {...password}
+              />
+              {password.value.length === 0 && (
+                <span className="text-sm text-red-500">
+                  Please enter your password
+                </span>
+              )}
+            </li>
 
-                <li className="my-4 flex justify-center items-center">
-                  <button
-                    data-testid="sign-in-button"
-                    type="submit"
-                    className="flex-grow h-[70px] bg-green-500 border-2 border-green-500 rounded-xl text-xl font-bold text-white
+            <li className="mt-4 sm:my-4 flex justify-center items-center">
+              <button
+                data-testid="sign-in-button"
+                type="submit"
+                className="flex-grow py-3 sm:py-4 bg-green-500 border-2 border-green-500 rounded-xl text-lg sm:text-xl font-bold text-white
                 hover:bg-green-600 focus:bg-green-600 focus:outline-none focus:ring-2 focus:ring-sky-400 active:scale-95 transition"
-                  >
-                    Sign In
-                  </button>
-                </li>
+              >
+                Sign In
+              </button>
+            </li>
 
-                <li className="my-4 flex flex-col justify-center items-center">
-                  <span className="text-md font-bold text-slate-200">
-                    Don't have an account?
-                  </span>
-                  <button
-                    onClick={handleClickSignUp}
-                    type="button"
-                    data-testid="sign-up-button"
-                    className="w-full flex-grow h-[70px] border-2 border-slate-200 rounded-xl text-xl font-bold text-slate-200
+            <li className="mt-4 sm:my-4 flex flex-col justify-center items-center">
+              <span className="text-sm sm:text-base font-bold text-slate-200">
+                Don't have an account?
+              </span>
+              <button
+                onClick={handleClickSignUp}
+                type="button"
+                data-testid="sign-up-button"
+                className="w-full flex-grow py-3 sm:py-4 border-2 border-slate-200 rounded-xl text-lg sm:text-xl font-bold text-slate-200
                 hover:bg-slate-500 focus:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-sky-400 active:scale-95 transition"
-                  >
-                    Sign Up Here
-                  </button>
-                </li>
-              </ul>
-            </form>
-          </div>
-        </div>
+              >
+                Sign Up Here
+              </button>
+            </li>
+          </ul>
+        </form>
       </div>
     </div>
   );
