5ad71bbb4dd567c767728b6d3e4ff4a0835265e9
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-08-03 23:09:56 +0300

    chore(backend): connect to database on server start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,13 @@
 import app from "./app.ts";
 import config from "../config.ts";
+import { db } from "./db/index.ts";
 
-app.listen(config.PORT, () => {
-  console.log(`Server is running on port ${config.PORT}`);
-});
+const start = async () => {
+  await db.execute("select 1");
+
+  app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+  });
+};
+
+start();
