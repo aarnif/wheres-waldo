b684600f160245a5d60ffa565fa1d7950c807d5b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-08-04 11:34:00 +0300

    chore(db): enable Drizzle relational queries

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import config from "../../config.ts";
+import * as schema from "./schema.ts";
 
-export const db = drizzle(config.DATABASE_URL);
+export const db = drizzle(config.DATABASE_URL, { schema });
