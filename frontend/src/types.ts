63072cf60ee24f0b07ebe402033fd06b3ccb79d6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-08-09 13:57:18 +0300

    feat(frontend): add login service function

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -24,3 +24,12 @@ export interface DecodedToken {
   id: number;
   username: string;
 }
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+}
