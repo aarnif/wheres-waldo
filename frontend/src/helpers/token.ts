2d55c82a24b2ddbfb3ede883c83e436e316043c9
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-08-09 13:27:57 +0300

    chore(frontend): add token storage helper

---
+++ frontend/src/helpers/token.ts
@@ -0,0 +1,8 @@
+const TOKEN_KEY = "wheres-waldo-token";
+
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
+
+export const setToken = (token: string) =>
+  localStorage.setItem(TOKEN_KEY, token);
+
+export const clearToken = () => localStorage.removeItem(TOKEN_KEY);
