7fe5b03010047081f2b7a69cde857d4689877d1a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-08-24 16:34:08 +0300

    test(frontend): skip motion animations in tests

--- frontend/testSetup.ts
+++ frontend/testSetup.ts
@@ -1,6 +1,9 @@
 import "@testing-library/jest-dom/vitest";
+import { MotionGlobalConfig } from "motion";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
+
+MotionGlobalConfig.skipAnimations = true;
 
 afterEach(() => {
   cleanup();
